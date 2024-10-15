c9308bfd2cc8fbf85b4169ca5e1825c159748651
d0x471b <0x471@protonmail.com>
2024-10-15 12:54:22 +0100

    remove unrelated comment

--- ignition/modules/TokenBridge.ts
+++ ignition/modules/TokenBridge.ts
@@ -2,8 +2,5 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("TokenBridge", (m) => {
   const tokenBridge = m.contract("TokenBridge", []);
-
-  //m.call(tokenBridge, "launch", []);
-
   return { tokenBridge };
 });
